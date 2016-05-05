e5ac2d308d6bcb7e1534c7c25ec27dbb55632e1c
Vu Xuan Thanh <vuthanh20286@gmail.com>
2016-05-05 08:43:12 +0700

    Commit change

---
+++ CefSharp.WinForms.Example/Handlers/SchemaHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefSharp.WinForms.Example.Handlers
+{
+	class SchemaHandler : IResourceHandler
+	{
+		private string mimeType;
+		private MemoryStream stream = new MemoryStream();
+
+		public bool ProcessRequest(IRequest request, ICallback callback)
+		{
+			// NOTE: We suggest you structure your code in an async fashion
+			// First examine the "request" object for info about the URI being requested and so forth.
+			// If the Url is valid then spawn a task
+			var uri = new Uri(request.Url);
+			var fileName = uri.AbsolutePath;
+			Task.Run(() =>
+			{
+				// In this task you can perform your time consuming operations, e.g query a database
+				// NOTE: We suggest that you wrap callbacks in a using statemnt so that they're disposed
+				// even if there is an exception as they wrap an unmanaged response which will cause memory
+				// leaks if not freed
+				using (callback)
+				{
+					// Read the data in, set the mime type					
+
+					//stream = (MemoryStream) request.;
+					var fileExtension = Path.GetExtension(fileName);
+					mimeType = ResourceHandler.GetMimeType(fileExtension);
+
+					// When your finished processing execute the callback.
+					// Most callbacks have multiple methods, so checkout their interface for details
+					callback.Continue();
+				}
+			});
+
+			// Return true to indicate that you've handled the request, you can return false which will cancel the request
+			return true;
+		}
+
+		public void GetResponseHeaders(IResponse response, out long responseLength, out string redirectUrl)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool ReadResponse(System.IO.Stream dataOut, out int bytesRead, ICallback callback)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool CanGetCookie(Cookie cookie)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool CanSetCookie(Cookie cookie)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Cancel()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CefSharp.WinForms.Example/Handlers/SchemaHandlerFactory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace CefSharp.WinForms.Example.Handlers
+{
+	class SchemaHandlerFactory : ISchemeHandlerFactory
+	{
+		private const string SCHEMA_PREFIX = "custom://";
+		private const string BDS_SCHEMA = "batdongsan.com.vn/nha-dat-ban-tp-hcm";
+		private const string RB_SCHEMA = "rongbay.com/TP-HCM/Mua-Ban-nha-dat-c15.html";
+		private const string VG_SCHEMA = "rongbay.com/TP-HCM/Mua-Ban-nha-dat-c15.html";
+		private const string VN_SCHEMA = "rongbay.com/TP-HCM/Mua-Ban-nha-dat-c15.html";
+
+		private Dictionary<string, string> _schemaFactories = null;
+
+
+		public SchemaHandlerFactory()
+		{
+			InitSchema();
+		}
+
+		private void InitSchema()
+		{
+			_schemaFactories = new Dictionary<string, string>();
+			_schemaFactories.Add("BDS_SCHEMA", SCHEMA_PREFIX + BDS_SCHEMA);
+			_schemaFactories.Add("RB_SCHEMA", SCHEMA_PREFIX + RB_SCHEMA);
+			_schemaFactories.Add("VG_SCHEMA", SCHEMA_PREFIX + VG_SCHEMA);
+			_schemaFactories.Add("VN_SCHEMA", SCHEMA_PREFIX + VN_SCHEMA);		
+				
+		}
+
+		private bool isExistingSchema(string schemaName)
+		{
+
+			if (_schemaFactories == null) return false;
+			string tmpOut = string.Empty;
+			return _schemaFactories.TryGetValue(schemaName, out tmpOut);
+		}
+
+		public IResourceHandler Create(IBrowser browser, IFrame frame, string schemeName, IRequest request)
+		{
+			if (isExistingSchema(schemeName) &&
+				request.Url.EndsWith(BDS_SCHEMA, System.StringComparison.OrdinalIgnoreCase))
+			{
+				//Display the debug.log file in the browser
+				return ResourceHandler.FromFileName(BDS_SCHEMA);
+			}
+			else if (isExistingSchema(schemeName) &&
+				request.Url.EndsWith(RB_SCHEMA, System.StringComparison.OrdinalIgnoreCase))
+			{
+				return ResourceHandler.FromFileName(RB_SCHEMA);
+			}
+			else if (isExistingSchema(schemeName) &&
+				request.Url.EndsWith(VG_SCHEMA, System.StringComparison.OrdinalIgnoreCase))
+			{
+				return ResourceHandler.FromFileName(VG_SCHEMA);
+			}
+			else if (isExistingSchema(schemeName) &&
+				request.Url.EndsWith(VN_SCHEMA, System.StringComparison.OrdinalIgnoreCase))
+			{
+				return ResourceHandler.FromFileName(VN_SCHEMA);
+			}
+			return null;
+		}
+	}
+}
